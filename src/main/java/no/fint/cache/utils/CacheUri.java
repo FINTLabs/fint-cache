d5b80275307fdbb8dfeabfc54d6e83e616945ff4
jarlehansen <jarle@jarlehansen.net>
2017-06-29 12:35:03 +0200

    removed cacheUris map, use getKeys() on the CacheService instead

--- src/main/java/no/fint/cache/utils/CacheUri.java
+++ src/main/java/no/fint/cache/utils/CacheUri.java
@@ -1,12 +1,8 @@
 package no.fint.cache.utils;
 
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Based on rfc2141: https://www.ietf.org/rfc/rfc2141.txt
@@ -14,16 +10,11 @@ import java.util.Set;
 @Slf4j
 public class CacheUri {
 
-    @Getter
-    private static final Set<String> cacheUris = new HashSet<>();
-
     public static String create(String orgId, String model) {
         if (StringUtils.isEmpty(model)) {
             throw new IllegalArgumentException("Model must be set on cache implementation");
         }
 
-        String cacheUri = String.format("urn:fint.no:%s:%s", orgId, model);
-        cacheUris.add(cacheUri);
-        return cacheUri;
+        return String.format("urn:fint.no:%s:%s", orgId, model);
     }
 }
--- src/test/groovy/no/fint/cache/utils/CacheUriSpec.groovy
+++ src/test/groovy/no/fint/cache/utils/CacheUriSpec.groovy
@@ -4,17 +4,12 @@ import spock.lang.Specification
 
 class CacheUriSpec extends Specification {
 
-    void setup() {
-        CacheUri.cacheUris.clear()
-    }
-
     def "Get cache uri from model and orgId"() {
         when:
         def cacheUri = CacheUri.create('rogfk.no', 'personalressurs')
 
         then:
         cacheUri == 'urn:fint.no:rogfk.no:personalressurs'
-        CacheUri.cacheUris.size() == 1
     }
 
     def "Throw IllegalArgumentException if model is null"() {
